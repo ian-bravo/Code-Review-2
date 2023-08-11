78408d37fc8cf6cc59542e02ad50a91fdc6acb58
ian-bravo <bravo.ian@gmail.com>
2023-08-11 11:42:08 -0700

    simplify HTML questions and JS functions for debugging.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,33 @@
 //Business Logic
-function surveyCalculation (question1, question2, question3, question4, question5) {
+// function surveyCalculation (question1, question2, question3, question4, question5) {
+function surveyCalculation (question1) {
     let response;
-    if
+    if (document.querySelector('input[id="yellow"]:checked')); {
+        response = "Python"
+    } 
 
 }
 
-
-
-
-
-
-
-
 //User Logic
+function userSubmitResponse(event) {
 
-const questionOneResponse = document.querySelector('input[name="color"]:checked')
-const questionTwoResponse = document.querySelector('input[name="animal"]:checked')
-const questionThreeResponse = document.querySelector('input[name="disaster"]:checked')
-const questionFourResponse = document.querySelector('input[name="song"]:checked')
-const questionFiveResponse = document.querySelector('input[name="movie"]:checked')
+    event.preventDefault();
+const questionOneResponse = document.querySelector('input[name="color"]:checked').value
+// const questionTwoResponse = document.querySelector('input[name="animal"]:checked').value
+// const questionThreeResponse = document.querySelector('input[name="disaster"]:checked').value
+// const questionFourResponse = document.querySelector('input[name="song"]:checked').value
+// const questionFiveResponse = document.querySelector('input[name="movie"]:checked').value
 
-let showResults = surveyCalculation(questionOneResponse, questionTwoResponse, questionThreeResponse, questionFourResponse, questionFiveResponse);
+// let showResults = surveyCalculation(questionOneResponse, questionTwoResponse, questionThreeResponse, questionFourResponse, questionFiveResponse);
+let showResults = surveyCalculation(questionOneResponse);
 document.getElementById("survey-results").innerText = showResults;
 
+event.preventDefault();
+}
+
+function loadForm () {
+    let userSelectedQuestions = document.querySelector("#radio-form-q1-color");
+    userSelectedQuestions.addEventListener("submit", userSubmitResponse);
+}
+
+window.addEventListener("load", loadForm);
