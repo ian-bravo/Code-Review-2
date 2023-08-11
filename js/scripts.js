8d78f2c5b863a9d6e24dcd9c799c346b40933142
ian-bravo <bravo.ian@gmail.com>
2023-08-11 10:56:55 -0700

    update JS and add a button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 //Business Logic
-function surveyCalculation (question1, question2, question3, question4, question5)
+function surveyCalculation (question1, question2, question3, question4, question5) {
+    let response;
+    if
+
+}
 
 
 
@@ -9,3 +13,13 @@ function surveyCalculation (question1, question2, question3, question4, question
 
 
 //User Logic
+
+const questionOneResponse = document.querySelector('input[name="color"]:checked')
+const questionTwoResponse = document.querySelector('input[name="animal"]:checked')
+const questionThreeResponse = document.querySelector('input[name="disaster"]:checked')
+const questionFourResponse = document.querySelector('input[name="song"]:checked')
+const questionFiveResponse = document.querySelector('input[name="movie"]:checked')
+
+let showResults = surveyCalculation(questionOneResponse, questionTwoResponse, questionThreeResponse, questionFourResponse, questionFiveResponse);
+document.getElementById("survey-results").innerText = showResults;
+
