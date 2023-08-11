775e580116d3a41d675bf7a351f78a9bf1ef8260
ian-bravo <bravo.ian@gmail.com>
2023-08-11 15:45:41 -0700

    continue to debug business function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,40 @@
 //Business Logic
 // function surveyCalculation (question1, question2, question3, question4, question5) {
-function surveyCalculation (question1) {
-    let response;
-    if (document.querySelector('input[id="color-yellow"]:checked') === true) {
-        response = "Python";
-    } else {
-        response = "try again";
+    function surveyCalculation (question1) {
+        let response;
+        if (question1 === "yellow") {            
+            response = "Python";
+        } else {
+            response = "try again";
+        }
+        return response;    
     }
-    return response;    
-}
+    
+
+// function surveyCalculation (question1) {
+//     let response;
+//     if (document.querySelector('input[id="color-yellow"]:checked') === true) {
+//         response = "Python";
+//     } else {
+//         response = "try again";
+//     }
+//     return response;    
+// }
 
 //User Logic
 function userSubmitResponse(event) {
     event.preventDefault();
-    const questionOneResponse = document.querySelector("input[name='color']:checked").value;
+    
+    const color = document.querySelector("input[name='color']:checked").value;
 // const questionTwoResponse = document.querySelector('input[name="animal"]:checked').value
 // const questionThreeResponse = document.querySelector('input[name="disaster"]:checked').value
 // const questionFourResponse = document.querySelector('input[name="song"]:checked').value
 // const questionFiveResponse = document.querySelector('input[name="movie"]:checked').value
 
 // let showResults = surveyCalculation(questionOneResponse, questionTwoResponse, questionThreeResponse, questionFourResponse, questionFiveResponse);
-let showResults = surveyCalculation(questionOneResponse);
+let showResults = surveyCalculation(color);
 document.getElementById("survey-results").innerText = showResults;
 
-event.preventDefault();
 }
 
 function loadForm () {
