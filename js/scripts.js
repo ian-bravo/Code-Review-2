b7d1c003b95d8dd2ad8fbe214aec1238d74620d0
ian-bravo <bravo.ian@gmail.com>
2023-08-11 16:14:55 -0700

    debug the business logic function!

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,35 @@
 //Business Logic
-// function surveyCalculation (question1, question2, question3, question4, question5) {
-    function surveyCalculation (question1) {
-        let response;
-        if (question1 === "yellow") {            
-            response = "Python";
-        } else {
-            response = "try again";
-        }
-        return response;    
+function surveyCalculation (colorQuestion, animalQuestion, disasterQuestion, songQuestion, movieQuestion) {
+    let response;
+    if (colorQuestion === "yellow" && animalQuestion === "kitten" && disasterQuestion === "fog" && songQuestion === "oops" && movieQuestion === "nightmare") {            
+        response = "Your first programming language is... Python!";
+    } else {
+        response = "try again";
     }
+return response;    
+}
     
 
-// function surveyCalculation (question1) {
-//     let response;
-//     if (document.querySelector('input[id="color-yellow"]:checked') === true) {
-//         response = "Python";
-//     } else {
-//         response = "try again";
-//     }
-//     return response;    
-// }
+
 
 //User Logic
 function userSubmitResponse(event) {
     event.preventDefault();
     
-    const color = document.querySelector("input[name='color']:checked").value;
-// const questionTwoResponse = document.querySelector('input[name="animal"]:checked').value
-// const questionThreeResponse = document.querySelector('input[name="disaster"]:checked').value
-// const questionFourResponse = document.querySelector('input[name="song"]:checked').value
-// const questionFiveResponse = document.querySelector('input[name="movie"]:checked').value
+    const colorChoice = document.querySelector("input[name='color']:checked").value;
+    const animalChoice = document.querySelector('input[name="animal"]:checked').value;
+    const disasterChoice = document.querySelector('input[name="disaster"]:checked').value;
+    const songChoice = document.querySelector('input[name="song"]:checked').value;
+    const movieChoice = document.querySelector('input[name="movie"]:checked').value;
 
-// let showResults = surveyCalculation(questionOneResponse, questionTwoResponse, questionThreeResponse, questionFourResponse, questionFiveResponse);
-let showResults = surveyCalculation(color);
+let showResults = surveyCalculation(colorChoice, animalChoice, disasterChoice, songChoice, movieChoice);
 document.getElementById("survey-results").innerText = showResults;
 
 }
 
 function loadForm () {
-    let userSelectedQuestions = document.querySelector("#radio-form-q1-color");
-    userSelectedQuestions.addEventListener("submit", userSubmitResponse);
+    let userSelectedAnswers = document.querySelector("#survey-questions");
+    userSelectedAnswers.addEventListener("submit", userSubmitResponse);
 }
 
 window.addEventListener("load", loadForm);
