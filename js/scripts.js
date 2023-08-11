7d900f2eec22ca42fd114de05f8092b4935a2335
ian-bravo <bravo.ian@gmail.com>
2023-08-11 12:30:46 -0700

    continue to debug function. WIP

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,18 @@
 // function surveyCalculation (question1, question2, question3, question4, question5) {
 function surveyCalculation (question1) {
     let response;
-    if (document.querySelector('input[id="yellow"]:checked')); {
-        response = "Python"
-    } 
-
+    if (document.querySelector('input[id="color-yellow"]:checked') === true) {
+        response = "Python";
+    } else {
+        response = "try again";
+    }
+    return response;    
 }
 
 //User Logic
 function userSubmitResponse(event) {
-
     event.preventDefault();
-const questionOneResponse = document.querySelector('input[name="color"]:checked').value
+    const questionOneResponse = document.querySelector("input[name='color']:checked").value;
 // const questionTwoResponse = document.querySelector('input[name="animal"]:checked').value
 // const questionThreeResponse = document.querySelector('input[name="disaster"]:checked').value
 // const questionFourResponse = document.querySelector('input[name="song"]:checked').value
